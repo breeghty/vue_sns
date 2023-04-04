85429774ace84dcca1742c040cf68c0767e7f741
breeghty <kal7411@kakao.com>
2023-04-04 14:10:27 +0900

    blog

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header">
+    <ul class="header-button-left">
+      <li>Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li>Next</li>
+    </ul>
+    <img src="./assets/logo.png" class="logo" alt="로고입니다.">
+  </div>
+
+  <ContainerBox></ContainerBox>
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input type="file" id="file" class="inputfile">
+      <label for="file" class="input-plus">+</label>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import ContainerBox from './components/Container.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ContainerBox: ContainerBox,
+
   }
 }
 </script>
 
 <style>
+@import 'style.css';
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+box-sizing: border-box;
+font-family: "consolas";
+margin-top: 60px;
+width: 100%;
+max-width: 460px;
+margin: auto;
+position: relative;
+border-right: 1px solid #eee;
+border-left: 1px solid #eee;
 }
 </style>
---
+++ src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <postSns></postSns>
+        <postSns></postSns>
+        <postSns></postSns>
+    </div>
+</template>
+
+<script>
+import postSns from './Post.vue';
+
+export default {
+    name: 'ContainerBox',
+    components: {
+        postSns: postSns,
+    },
+}
+</script>
+
+<style>
+
+</style>
